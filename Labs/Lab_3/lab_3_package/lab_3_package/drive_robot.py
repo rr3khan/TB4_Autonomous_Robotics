40816e56ed6e66161620021a54dbb3a99324e442
Riyad <rr3khan@uwaterloo.ca>
2022-11-29 22:08:59 -0800

    removed repeated update function

--- Labs/Lab_3/lab_3_package/lab_3_package/drive_robot.py
+++ Labs/Lab_3/lab_3_package/lab_3_package/drive_robot.py
@@ -143,67 +143,59 @@ class Driver(Node):
         vel_msg.angular.z = 0.0
         self.vel_publisher.publish(vel_msg)
     
-    # P controller
-
-    def update_pose(self, data):
-           """Callback function which is called when a new message of type Pose is
-           received by the subscriber."""
-           self.pose = data
-           self.pose.x = round(self.pose.x, 4)
-           self.pose.y = round(self.pose.y, 4)
    
-    def euclidean_distance(self, goal_pose):
-        return sqrt(pow((goal_pose.x - self.pose.x), 2) +
-                    pow((goal_pose.y - self.pose.y), 2))
+    # def euclidean_distance(self, goal_pose):
+    #     return sqrt(pow((goal_pose.x - self.pose.x), 2) +
+    #                 pow((goal_pose.y - self.pose.y), 2))
 
-    def linear_vel(self, goal_pose, constant=1.5):
-        return constant * self.euclidean_distance(goal_pose)
+    # def linear_vel(self, goal_pose, constant=1.5):
+    #     return constant * self.euclidean_distance(goal_pose)
 
-    def steering_angle(self, goal_pose):
-        return atan2(goal_pose.y - self.pose.y, goal_pose.x - self.pose.x)
+    # def steering_angle(self, goal_pose):
+    #     return atan2(goal_pose.y - self.pose.y, goal_pose.x - self.pose.x)
 
-    def angular_vel(self, goal_pose, constant=6):
-        return constant * (self.steering_angle(goal_pose) - self.pose.theta)
+    # def angular_vel(self, goal_pose, constant=6):
+    #     return constant * (self.steering_angle(goal_pose) - self.pose.theta)
 
-    def move2goal(self):
-        """Moves the turtle to the goal."""
-        print("Move to goal")
-        goal_pose = Pose()
+    # def move2goal(self):
+    #     """Moves the turtle to the goal."""
+    #     print("Move to goal")
+    #     goal_pose = Pose()
 
-        # Get the input from the user.
-        goal_pose.x = float(input("Set your x goal: "))
-        goal_pose.y = float(input("Set your y goal: "))
+    #     # Get the input from the user.
+    #     goal_pose.x = float(input("Set your x goal: "))
+    #     goal_pose.y = float(input("Set your y goal: "))
 
-        # Please, insert a number slightly greater than 0 (e.g. 0.01).
-        distance_tolerance = float(input("Set your tolerance: "))
+    #     # Please, insert a number slightly greater than 0 (e.g. 0.01).
+    #     distance_tolerance = float(input("Set your tolerance: "))
 
-        vel_msg = Twist()
+    #     vel_msg = Twist()
 
-        while self.euclidean_distance(goal_pose) >= distance_tolerance:
+    #     while self.euclidean_distance(goal_pose) >= distance_tolerance:
 
-            # Porportional controller.
-            # https://en.wikipedia.org/wiki/Proportional_control
+    #         # Porportional controller.
+    #         # https://en.wikipedia.org/wiki/Proportional_control
 
-            # Linear velocity in the x-axis.
-            vel_msg.linear.x = self.linear_vel(goal_pose)
-            vel_msg.linear.y = 0.0
-            vel_msg.linear.z = 0.0
+    #         # Linear velocity in the x-axis.
+    #         vel_msg.linear.x = self.linear_vel(goal_pose)
+    #         vel_msg.linear.y = 0.0
+    #         vel_msg.linear.z = 0.0
 
-            # Angular velocity in the z-axis.
-            vel_msg.angular.x = 0.0
-            vel_msg.angular.y = 0.0
-            vel_msg.angular.z = self.angular_vel(goal_pose)
+    #         # Angular velocity in the z-axis.
+    #         vel_msg.angular.x = 0.0
+        #     vel_msg.angular.y = 0.0
+        #     vel_msg.angular.z = self.angular_vel(goal_pose)
 
-            # Publishing our vel_msg
-            self.vel_publisher.publish(vel_msg)
+        #     # Publishing our vel_msg
+        #     self.vel_publisher.publish(vel_msg)
 
-            # Publish at the desired rate.
-            # self.rate.sleep()
+        #     # Publish at the desired rate.
+        #     # self.rate.sleep()
 
-        # Stopping our robot after the movement is over.
-        vel_msg.linear.x = 0.0
-        vel_msg.angular.z = 0.0
-        self.vel_publisher.publish(vel_msg)
+        # # Stopping our robot after the movement is over.
+        # vel_msg.linear.x = 0.0
+        # vel_msg.angular.z = 0.0
+        # self.vel_publisher.publish(vel_msg)
     
     def log(self):
         print(self.pose)
